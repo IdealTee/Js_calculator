04b4de4e2835aad41c06858a9a9a6d6be93878ff
IdealTee <morakoluwatosin772@gmail.com>
2022-06-24 13:32:47 -0700

    initialcommit

---
+++ index.js
@@ -0,0 +1,16 @@
+let num1 = parseFloat(parseInt(prompt('Enter the first number')));
+let num2 = parseFloat(parseInt(prompt('Enter the second number')));
+
+let sum = (num1+num2);
+let subtraction = (num1-num2);
+let multiplication = (num1*num2);
+let division = (num1/num2);
+let modulus = (num1%num2);
+
+alert('The sum of the given number is: ' +sum);
+alert('The difference of the given numbers is: ' +subtraction);
+alert('The products of the given numbers is: ' +multiplication);
+alert('The division of the given numbers is: ' +division);
+alert('The remainder of the given numbers is: ' +modulus);
+
+alert('To do more calculations refresh page')
